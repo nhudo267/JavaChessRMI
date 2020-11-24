22abce84a3a7777f371a8ff6bb82af6b37e44947
nhudo267 <nhudo164@gmail.com>
2020-11-25 04:55:47 +0700

    mm

--- Server/GameSession.java
+++ Server/GameSession.java
@@ -69,5 +69,47 @@ public class GameSession extends UnicastRemoteObject implements GameSessionInter
 	{
 		board.nextPlayerTurn();
 	}
+	@Override
+	public void playerExit() throws RemoteException
+	{
+		if(playersInGame == 1)
+		{
+			playersInGame--;
+
+		}
+		else if(playersInGame == 2)
+		{
+			playersInGame--;
+		}
+		else
+		{
+			playersInGame = 0;
+		}
+
+	}
 	
+	@Override
+	public void resetBoard() throws RemoteException
+	{
+		board.populateBoard();
+	}
+	
+	@Override
+	public void setWinningPlayer(int p) throws RemoteException
+	{
+		board.setWinningPlayer(p);
+	
+	}
+	
+	@Override
+	public boolean swapPiece(int x1, int y1, int x2, int y2, int c) throws RemoteException
+	{
+		return board.swap(x1, y1, x2, y2, c);
+	}
+
+	@Override
+	public void setPlayerTurn(int t) throws RemoteException
+	{
+		board.setPlayerTurn(t);
+	}
 }
