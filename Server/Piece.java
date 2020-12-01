04ec87970ef97678bd93c3284291ddc856f7ac00
nguyen dep zai <nguyen.kv168756@sis.hust.edu.vn>
2020-12-02 00:35:38 +0700

    config position

--- Server/Piece.java
+++ Server/Piece.java
@@ -3,4 +3,12 @@ public class Piece {
 	private int positionY;
 	private int color;
 	private String pieceType;
+
+  //constructors and getters and setters
+	Piece(int c)
+	{
+		this.setPositionX(-1);
+		this.positionY =-1;
+		this.setColor(c);
+	}
 }
